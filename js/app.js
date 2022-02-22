c9244bb4393edffe96c8ffea88c6f30cbc109531
hongkpng854 <99008862+hongkpng854@users.noreply.github.com>
2022-02-23 04:23:38 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/build 20220223 on 2.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/20220223 on 3.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
